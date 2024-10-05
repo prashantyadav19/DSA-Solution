19c96866d23865a4c8c0b3a23e33687bb90d5607
prashant <prashant.yadav567@gmail.com>
2024-10-05 19:15:04 +0530

    Time: 126 ms (5.91%), Space: 57.6 MB (5.42%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function (matrix, target) {
+    let rows = matrix.length;
+    let cols = matrix[0].length;
+
+    let i = 0, j = rows*cols -1;
+    
+    while (i <= j ) {
+       let mid = Math.floor(i + ((j -i))/2);
+       let row = Math.floor(mid / cols)
+       let col = Math.floor(mid % cols)
+       console.log('mid----', mid)
+       console.log(row,'col----', col)
+        if (matrix[row][col] == target) {
+            return true
+        }else if (matrix[row][col] > target) {
+            j--;
+        } else {
+            i++;
+        }
+    }
+    return false;
+};
